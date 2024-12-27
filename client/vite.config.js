14bbcc11add8c769277399b0caabb0abdbabf9f5
Harsh Soni <algebra.soni@gmail.com>
2024-12-27 15:40:36 +0000

    config: bring back client/dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'  // Ensure this is pointing to the correct location
+    outDir: 'client/dist'  // Ensure this is pointing to the correct location
   }
 })
